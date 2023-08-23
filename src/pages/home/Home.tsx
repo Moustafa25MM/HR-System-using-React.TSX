ac1dc9a0db680cd9acad579d1f38286c8b7a0fde
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-08-23 19:21:10 +0300

    Pagination in Dashboard

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,17 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-
+import './Home.css';
 interface HrEmployee {
   email: string;
   group: string;
   name: string;
 }
 const Home = () => {
-  const [hrCount, setAdminCount] = useState();
+  const [hrCount, setAdminCount] = useState(0);
   const [employeeCount, setEmployeeCount] = useState();
   const [hrEmployees, setHrEmployees] = useState<HrEmployee[]>([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [pageSize, setPageSize] = useState(5);
+  const [totalDocs, setTotalDocs] = useState(0);
 
   const usenavigate = useNavigate();
   let token = localStorage.getItem('token');
@@ -21,19 +25,28 @@ const Home = () => {
     }
   });
 
-  useEffect(() => {
+  const loadHrEmployees = () => {
     axios
-      .get(`${process.env.REACT_APP_BASE_API_URL}employees/all?group=hr`, {
-        headers: {
-          Authorization: `${token}`,
-        },
-      })
+      .get(
+        `${process.env.REACT_APP_BASE_API_URL}employees/all?group=hr&pageNumber=${pageNumber}&pageSize=${pageSize}`,
+        {
+          headers: {
+            Authorization: `${token}`,
+          },
+        }
+      )
       .then((res) => {
+        console.log(res.data.employees);
         setAdminCount(res.data.pagination.totalDocs);
+        setTotalDocs(res.data.pagination.totalDocs);
         setHrEmployees(res.data.employees);
       })
       .catch((err) => console.log(err));
-
+  };
+  useEffect(() => {
+    loadHrEmployees();
+  }, [pageNumber]);
+  useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_BASE_API_URL}employees/all?group=normal`, {
         headers: {
@@ -44,7 +57,16 @@ const Home = () => {
         setEmployeeCount(res.data.pagination.totalDocs);
       })
       .catch((err) => console.log(err));
-  }, []);
+  });
+  const handleNext = () => {
+    setPageNumber(pageNumber + 1);
+  };
+
+  const handlePrev = () => {
+    if (pageNumber > 1) {
+      setPageNumber(pageNumber - 1);
+    }
+  };
   return (
     <div>
       <div className='p-3 d-flex justify-content-around mt-3'>
@@ -89,6 +111,30 @@ const Home = () => {
             </tr>
           ))}
         </table>
+        <div className='pagination-wrapper'>
+          <div className='pagination-buttons'>
+            <button
+              onClick={handlePrev}
+              className='btn btn-primary me-2'
+              disabled={pageNumber === 1}
+            >
+              Previous
+            </button>
+            <button
+              onClick={handleNext}
+              className='btn btn-primary'
+              disabled={totalDocs / pageSize <= pageNumber}
+            >
+              Next
+            </button>
+          </div>
+
+          <div className='page-info'>
+            <h5>
+              Page: {pageNumber} / {Math.ceil(totalDocs / pageSize)}
+            </h5>
+          </div>
+        </div>
       </div>
     </div>
   );
