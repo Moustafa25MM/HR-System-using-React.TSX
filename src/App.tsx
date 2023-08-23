c7710077ee4805b741a960019e6d26eb2f12c0e0
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-08-23 04:43:25 +0300

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <>
       <Router>
+        <NavBar />
         <Routes></Routes>
       </Router>
     </>
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+function NavBar() {
+  return (
+    <header className='navbar'>
+      <h3 className='navbar-logo'>HR System</h3>
+    </header>
+  );
+}
+
+export default NavBar;
